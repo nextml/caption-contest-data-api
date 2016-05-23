b7c33e88a31306db2c08fcd8505ddcc3063ce320
Scott Sievert <me@scottsievert.com>
2016-05-23 14:28:19 -0500

    publishes stats for adaptive-only contests

--- summary-stats.py
+++ summary-stats.py
@@ -27,6 +27,9 @@ def print_summary_file_for_contest(captions_file, query_file, summary_file,
             if algorithm.lower() in alg_label.lower() or \
                     alg_label.lower() in algorithm.lower():
                 rating = int(float(rating))
+                if not caption in captions.keys():
+                    captions[caption] = {'Unfunny':0, 'Somewhat funny':0,
+                            'Funny':0}
                 captions[caption][value_rating[rating]] += 1
 
     with open(summary_file, 'w') as f:
@@ -44,19 +47,19 @@ algorithm = 'LilUCB'
 
 # print_summary_file_for_contest(captions_file, query_file, summary_file)
 
-for exp in os.listdir('contests/'):
-    if 'DS' in exp or '.md' in exp or 'adaptive' in exp:
-        continue
-    if int(exp) < 510:
-        continue
-    if 'participant-responses.csv' in os.listdir('contests/' + exp):
-        exp_dir = 'contests/' + exp + '/'
-        captions_file = exp_dir + exp + '_captions.txt'
-        query_file = exp_dir + 'participant-responses.csv'
-        summary_file = exp + '_summary_{}.csv'.format(algorithm)
+# for exp in os.listdir('contests/'):
+    # if 'DS' in exp or '.md' in exp or 'adaptive' in exp:
+        # continue
+    # if int(exp) < 510:
+        # continue
+    # if 'participant-responses.csv' in os.listdir('contests/' + exp):
+        # exp_dir = 'contests/' + exp + '/'
+        # captions_file = exp_dir + exp + '_captions.txt'
+        # query_file = exp_dir + 'participant-responses.csv'
+        # summary_file = exp + '_summary_{}.csv'.format(algorithm)
 
-        print_summary_file_for_contest(captions_file, query_file, summary_file,
-                                       algorithm=algorithm)
+        # print_summary_file_for_contest(captions_file, query_file, summary_file,
+                                       # algorithm=algorithm)
 
 for exp in os.listdir('contests/adaptive-only-contests/'):
     if 'DS' in exp or '.md' in exp:
