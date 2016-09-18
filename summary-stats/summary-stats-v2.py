82f1b8fd1dad3baf004bfd97726989548252e22d
Scott Sievert <me@scottsievert.com>
2016-09-18 10:29:26 -0500

    DATA: publishes 536
    Former-commit-id: 22d7d118f209a48e54b1ae8a29c184f86f0b471f

--- summary-stats/summary-stats-v2.py
+++ summary-stats/summary-stats-v2.py
@@ -20,7 +20,7 @@ def find_score_prec(s):
 
 
 if True:
-    contest = '535'
+    contest = '536'
     csv = pd.read_csv('./{}_summary.csv'.format(contest))
     del csv['email']
     #  csv.to_excel('./531_summary.xlsx')
