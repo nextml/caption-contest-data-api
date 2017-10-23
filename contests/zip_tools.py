ba7a930c7ce8d9d21d64712fba8467071c4b4b02
Liam Marshall <limarshall@wisc.edu>
2017-10-23 13:55:03 -0500

    zip_tools.py: better argument handling

--- contests/zip_tools.py
+++ contests/zip_tools.py
@@ -15,10 +15,9 @@ import os
 import sys
 from subprocess import DEVNULL, call
 
+USAGE = 'USAGE: python zip_util.py [zip | unzip]'
 
-def cli():
-    operation = sys.argv[1]
-
+def cli(operation):
     if call('git lfs env', shell=True,
                        stdout=DEVNULL, stderr=DEVNULL) != 0:
         print('''Git LFS is not installed.
@@ -33,7 +32,7 @@ Install Git LFS and run `git lfs pull` in this repository to pull the contents o
                 continue
             os.system(f'cd {in_dir}; zip {file}.zip {file}; mv {file}.zip ../{out_dir}')
 
-    if operation == 'unzip':
+    elif operation == 'unzip':
         in_dir = 'responses'
         out_dir = 'responses-unzipped'
         for file in os.listdir(in_dir):
@@ -43,6 +42,14 @@ Install Git LFS and run `git lfs pull` in this repository to pull the contents o
             filename = file[:-4]
             os.system(f'cd {in_dir}; unzip {file}; mv {filename} ../{out_dir}')
 
+    else:
+        print(USAGE)
+        sys.exit(2)
+
 
 if __name__ == "__main__":
-    cli()
+    if len(sys.argv) != 2:
+        print(USAGE)
+        sys.exit(2)
+
+    cli(sys.argv[1])
