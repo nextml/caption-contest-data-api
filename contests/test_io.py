530b766a2aff71b18f92da92ae76d39110792543
Scott Sievert <github@stsievert.com>
2018-04-01 18:09:07 -0500

    MAINT: be more explicit about which columns to expect in responses

--- contests/test_io.py
+++ contests/test_io.py
@@ -1,15 +1,17 @@
 import os, sys
 import numpy as np
 import pandas as pd
-contest_dir = '/'.join(__file__.split('/')[:-1])
-sys.path.append('{contest_dir}/../example-analyses'.format(contest_dir=contest_dir))
+contest_dir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+sys.path.append(contest_dir + '/../example-analyses')
 import utils
 
 
 def test_responses():
     dir = '{contest_dir}/responses/'.format(contest_dir=contest_dir)
     exp_ids = []
-    last_header = None
+    expected_columns = {'network_delay', 'participant_uid', 'response_time',
+                        'target', 'target_id', 'target_reward',
+                        'timestamp_query_generated', 'alg_label'}
     for file in os.listdir(dir):
         if 'DS_Store' in file or '.md' in file or os.path.isdir(dir + file):
             continue
@@ -18,9 +20,7 @@ def test_responses():
         df = utils.read_responses(file)
         exp_ids += [df.iloc[0]['participant_uid'].split('_')[0]]
 
-        if last_header is not None:
-            assert all(last_header == df.columns)
-            last_header = df.columns
+        assert set(df.columns) == expected_columns
 
     assert len(exp_ids) == len(set(exp_ids))
 
@@ -91,9 +91,9 @@ def test_add_target_ids_to_summary():
 
 
 if __name__ == "__main__":
-    # test_responses()
+    test_responses()
     # test_summaries()
-    test_calculate_stats()
+    #  test_calculate_stats()
     # test_stats_generation_from_responses()
     # test_summary_rank()
     # test_init_summary()
