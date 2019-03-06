f93e55e5c8f2dcdbbae43d5a46d7b42fbe0f1d84
Scott Sievert <stsievert@users.noreply.github.com>
2019-03-06 04:51:45 +0000

    TST: clean dataframes (#17)
    **What does this PR implement?**
    It makes sure that...
    
    * every dataframe has the same column names
    * every dataframe the expected score
    * test for no nulls in certain columns (and explicitly mentions how many nulls are in each column)
    * the funniest caption is at the top of the file
    
    It also re-ranks the data with values: captions with the same score have the same rank.
    
    Closes #15.

--- contests/test_add_click_data.py
+++ contests/test_add_click_data.py
@@ -5,13 +5,7 @@ import pandas as pd
 import pytest
 
 from add_data import recover_counts
-
-filenames = [
-    f
-    for f in os.listdir("summaries/")
-    if ".DS" not in f and f[0] != "_" and int(f[:3]) < 587
-]
-dfs = {name: pd.read_csv("summaries/" + name) for name in filenames}
+from test_summaries import df, filenames
 
 
 def poor_reconstruction(filename):
@@ -25,38 +19,16 @@ def test_mostly_good_reconstructions(filenames):
     assert len(bad) / len(filenames) < 0.20
 
 
-@pytest.mark.parametrize("filename, df", dfs.items())
-def test_recover(filename, df):
+def test_recover(df):
     new_df = recover_counts(df)
     assert new_df is not df
     assert set(df.index) == set(new_df.index)
-    assert {
-        # "rank",
-        "caption",
-        "score",
-        "precision",
-        # "count",
-        "unfunny",
-        "somewhat_funny",
-        "funny",
-    }.issubset(set(df.columns).union({"somewhat funny", "somewhat_funny"}))
     for col in df.columns:
         if df[col].dtype == object:
             assert list(new_df[col]) == list(df[col])
         elif col in {"funny", "unfunny", "somewhat_funny"}:
             diff = np.abs(new_df[col] - df[col])
-            if poor_reconstruction(filename):
+            if poor_reconstruction(df.filename):
                 assert np.abs(new_df[col] - df[col]).max() <= 4
             else:
                 assert np.allclose(new_df[col], df[col]) or diff.max() < 1e-7
-
-
-@pytest.mark.parametrize("filename, df", dfs.items())
-def test_counts(filename, df):
-    somewhat_funny = (
-        df["somewhat_funny"] if "somewhat_funny" in df else df["somewhat funny"]
-    )
-    predicted_score = (df["unfunny"] + 2 * somewhat_funny + 3 * df["funny"]) / df[
-        "count"
-    ]
-    assert np.allclose(df["score"], predicted_score)
