abc04ac2ddc7aa08459a35e21129705382b59609
Scott Sievert <stsievert@users.noreply.github.com>
2019-03-06 04:51:45 +0000

    TST: clean dataframes (#17)
    **What does this PR implement?**
    It makes sure that...
    
    * every dataframe has the same column names
    * every dataframe the expected score
    * test for no nulls in certain columns (and explicitly mentions how many nulls are in each column)
    * the funniest caption is at the top of the file
    
    It also re-ranks the data with values: captions with the same score have the same rank.
    
    Closes #15.

--- contests/test_summaries.py
+++ contests/test_summaries.py
@@ -1,44 +1,78 @@
 import pandas as pd
-import os
 import pytest
-import sys
+import numpy as np
+import os
+import scipy.stats
 
-sys.path.append("../example-analyses")
-import utils
-
-dfs = {f: pd.read_csv("summaries/" + f) for f in os.listdir("summaries/") if "csv" in f}
-df = utils.read_all_summaries()
-expected_cols = {
-    "rank",
-    "caption",
-    "score",
-    "precision",
-    "count",
-    "funny",
-    "somewhat_funny",
-    "unfunny",
-    "contest",
-}
+filenames = [
+    f
+    for f in os.listdir("summaries/")
+    if ".DS" not in f and f[0] != "_" and int(f[:3]) < 587
+]
 
 
-@pytest.mark.parametrize("f,df", list(dfs.items()))
-def test_summary(f, df, expected_cols=expected_cols):
-    cols = set(df.columns)
-    top = df["score"].idxmax()
-    bottom = df["score"].idxmin()
-    assert df.loc[top]["rank"] == 1  # fails for certain contests
-    assert expected_cols.issubset(cols), "Missing columns {} in {}".format(
-        expected_cols - cols, f
-    )  # a lot of dfs are missing column "contest"
-    assert df.loc[bottom]["rank"] == len(df)  # often fails; use scipy.stats.rankdata
+@pytest.fixture(params=filenames)
+def df(request):
+    filename = request.param
+    df = pd.read_csv("summaries/" + filename)
+    df.filename = filename
+    return df
 
 
-@pytest.mark.parametrize("df", [df])
-@pytest.mark.parametrize("col", list(expected_cols))
-def test_complete(df, col):
-    nulls = df[col].isnull().sum()
-    # need to reformulate a lot of columns
-    # score, precision have 2 nans
-    # somewhat_funny (" " vs "_"?)
-    # count is also null for some contests
-    assert nulls == 0, f"{col}"
+def test_columns(df):
+    rank_score = {r: s for r, s in zip(df["rank"], df["score"])}
+    ranks = sorted(list(rank_score.keys()))
+    ranks = np.array(ranks)
+    for k in rank_score:
+        if k >= 2:
+            i = (ranks == k).argmax()
+            if np.isnan(rank_score[k]):
+                continue
+            assert rank_score[ranks[i - 1]] >= rank_score[k]
+    expected_cols = {
+        "rank",
+        "caption",
+        "score",
+        "precision",
+        "count",
+        "unfunny",
+        "somewhat_funny",
+        "funny",
+    }
+    assert expected_cols == set(df.columns)
+
+    # Make sure the best caption comes first
+    assert df["rank"].iloc[0] == 1
+
+
+def test_counts(df):
+    expected_count = df["funny"] + df["somewhat_funny"] + df["unfunny"]
+    assert (df["count"] == expected_count).all()
+
+
+def test_means(df):
+    predicted_score = df["unfunny"] + 2 * df["somewhat_funny"] + 3 * df["funny"]
+    predicted_score /= df["count"]
+    nan = np.isnan(predicted_score) | df["score"].isnull()
+    assert np.allclose(df["score"][~nan], predicted_score[~nan])
+
+
+def test_few_nulls(df):
+    for col in df.columns:
+        nulls = df[col].isnull().sum()
+        if col == "caption":
+            assert nulls <= 3, "Sometimes people don't submit *anything*"
+        elif col in {"score", "precision"} and any(
+            x in df.filename for x in ["520", "521"]
+        ):
+            assert nulls == 1
+        else:
+            assert nulls == 0, f"{col}"
+
+
+if __name__ == "__main__":
+    dfs = {fname: pd.read_csv("summaries/" + fname) for fname in filenames}
+
+    for fname, df in dfs.items():
+        df = df.sort_values(by="rank")
+        #  df.to_csv("summaries/" + fname, index=False)
