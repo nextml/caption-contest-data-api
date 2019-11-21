e3d9d7baa3b6122efec0529607b7abd7dddcbadb
Scott Sievert <dev@stsievert.com>
2019-11-20 20:58:20 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="caption_contest_data",
+    version="0.1-alpha",
+    packages=find_packages(),
+    author="Scott Sievert",
+    author_email="caption@stsievert.com",
+    description="Data from The New Yorker Cartoon Caption Contest",
+    keywords="data humor rating contest",
+)
