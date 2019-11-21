28afa3a9f8af6bf3e7524957c4762552ec290df9
Scott Sievert <dev@stsievert.com>
2019-11-21 15:24:51 -0600

    deps

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
+import re, os, codecs
 from setuptools import setup
-from caption_contest_data import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    """
+    https://packaging.python.org/guides/single-sourcing-package-version/
+    """
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 with open("requirements.txt", "r") as f:
     packages = [x.strip("\n") for x in f.readlines()]
 
 setup(
     name="caption_contest_data",
-    version=__version__,
+    version=find_version("caption_contest_data/__init__.py"),
     description="Data from The New Yorker Cartoon Caption Contest",
     classifiers=[
         "Development Status :: 1 - Planning",
