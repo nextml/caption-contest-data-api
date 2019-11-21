d6a96d9d202647f61ce8baabd6b032f6121aeeda
Scott Sievert <dev@stsievert.com>
2019-11-20 21:14:53 -0600

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="caption_contest_data",
     version="0.1-alpha",
-    packages=find_packages(),
-    author="Scott Sievert",
-    author_email="caption@stsievert.com",
     description="Data from The New Yorker Cartoon Caption Contest",
-    keywords="data humor rating contest",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Utilities",
+    ],
+    keywords="data caption contest humor",
+    author="Scott Sievert",
+    author_email="captions@stsievert.com",
+    #  url="https://www.ettus.com/",
+    #  license="GPLv3",
+    package_dir={"": "caption_contest_data"},
+    packages=["caption_contest_data"],
+    install_requires=["numpy", "pandas", "fsspec", "requests"],
 )
